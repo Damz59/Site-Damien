1e492f8e3ae0a5d1e502758925fbcb3340e75f86
Damien Vandendorpe <damienvdh59@gmail.com>
2026-01-24 18:38:08 +0100

    Mise à jour des liens github et linked, ajout de la banniere, reglage bug du header et mise a jour du guide sur notion

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,15 @@
 import { Card } from 'react-bootstrap'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { useState } from "react"
 
 function Section() {
+    const [copied, setCopied] = useState(false)
+
+    const copyEmail = () => {
+    navigator.clipboard.writeText('damienvdh59@gmail.com')
+    setCopied(true)
+    setTimeout(() => setCopied(false), 2000)
+    }
     return (
         <aside className="d-flex flex-column mt-5 top-section">
             {/* Card 1 - Liens utiles */}
@@ -11,13 +20,17 @@ function Section() {
                 <Card.Body>
                     <ul className="list-unstyled mb-0">
                         <li className="mb-2">
-                            <a href="https://github.com/Damz59" target="_blank" rel="noopener noreferrer" className="text-decoration-none">🔗 Mon GitHub</a>
+                            <a href="https://github.com/Damz59" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
+                            <FaGithub className="me-1" /> Mon GitHub
+                            </a>
                         </li>
                         <li className="mb-2">
-                            <a href="https://www.linkedin.com/in/damien-vandendorpe-81617824a" target="_blank" rel="noopener noreferrer" className="text-decoration-none">🔗 Mon LinkedIn</a>
+                            <a href="https://www.linkedin.com/in/damien-vandendorpe-81617824a" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
+                            <FaLinkedin className="me-1" /> Mon LinkedIn
+                            </a>
                         </li>
                         <li className="mb-0">
-                            <a href="#" className="text-decoration-none">📄 Télécharger mon CV</a>
+                            <a href="/CV_2026_Dev_web.pdf" className="text-decoration-none" download> 📄 Télécharger mon CV</a>
                         </li>
                     </ul>
                 </Card.Body>
@@ -29,7 +42,15 @@ function Section() {
                     <h5 className="mb-0">Contact rapide</h5>
                 </Card.Header>
                 <Card.Body>
-                    <p className="small text-muted mb-1">📧 damienvdh59@gmail.com</p>
+                    <p className="small text-muted mb-1">
+                        📧  <span 
+                                onClick={copyEmail} 
+                                className="email-link"
+                                title="Cliquer pour copier">
+                                damienvdh59@gmail.com
+                            </span>
+                        {copied && <span className="text-success ms-2">✓ Copié!</span>}
+                    </p>
                     <p className="small text-muted mb-0">📍 Halluin, France</p>
                 </Card.Body>
             </Card>
